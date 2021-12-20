8929ba3c042aa59cc8c57d446b332938ed93a9d3
anndreeaagml <andr77c6@edu.easj.dk>
2021-12-20 22:37:44 +0100

    Added python code for the sensor

---
+++ main.py
@@ -0,0 +1,38 @@
+import requests
+import RPi.GPIO as GPIO
+import time
+
+data = {
+    "ID": "1alpha",
+    "IsLocked": True
+}
+
+r=requests.post('https://lockeyapi.azurewebsites.net/Sensor', data=data)
+print(r)
+
+a=requests.get('https://lockeyapi.azurewebsites.net/Sensor')
+print(a._content.decode("utf-8"))
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(4,GPIO.IN)
+
+#initialise a previous input variable to 0 (Assume no pressure applied)
+prev_input = 0
+try:
+    while True:
+        #take a reading
+        input = GPIO.input(4)
+        #if the last reading was low and this one high, alert us
+        if (prev_input > input):
+            new_data={"IsLocked":False}
+            data.update(new_data)
+            r=requests.post('https://lockeyapi.azurewebsites.net/Sensor', data=data)
+            print(r)
+            print("Unlocked")
+        #update previous input
+        prev_input = input
+        #slight pause
+        time.sleep(0.10)
+except KeyboardInterrupt:
+    pass
+finally:
+    GPIO.cleanup()
