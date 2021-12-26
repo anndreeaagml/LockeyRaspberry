914af953c358aa8def63de3e2a78457f41833707
anndreeaagml <andr77c6@edu.easj.dk>
2021-12-26 21:35:26 +0100

    Beautyfied python code

--- main.py
+++ main.py
@@ -1,54 +1,74 @@
+from RPi import GPIO
 import requests
-import RPi.GPIO as GPIO
 import time
-import re, uuid 
-data = {
-    "ID": ':'.join(re.findall('..', '%012x' % uuid.getnode())),
-    "IsLocked": True
-}
+import uuid
+import re 
 
 
-a=requests.get('https://lockeysuperserverdeluxe.azurewebsites.net/Sensor')
-print(a._content.decode("utf-8"))
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(4,GPIO.IN)
+API_URL = 'https://lockeysuperserverdeluxe.azurewebsites.net/Sensor'
+MAC_ADDRESS = ':'.join(re.findall('..', '%012x' % uuid.getnode()))
+DELAY = 0.1  # seconds
 
-prev_input = GPIO.input(4)
-if (prev_input==0): 
-    new_data={"IsLocked":False}
-    data.update(new_data)
-    r=requests.post('https://lockeysuperserverdeluxe.azurewebsites.net/Sensor', data=data)
-    print(r)
-    print("Unocked")
-else:
-    new_data={"IsLocked":True}
-    data.update(new_data)
-    r=requests.post('https://lockeysuperserverdeluxe.azurewebsites.net/Sensor', data=data)
-    print(r)
-    print("Locked")
-try:
-    while True:
-        #take a reading
-        input = GPIO.input(4)
-        print(input)
-        #if the last reading was low and this one high, alert us
-        if (prev_input < input):
-            new_data={"IsLocked":True}
-            data.update(new_data)
-            r=requests.post('https://lockeysuperserverdeluxe.azurewebsites.net/Sensor', data=data)
-            print(r)
-            print("Locked")
-        if (prev_input > input):
-            new_data={"IsLocked":False}
-            data.update(new_data)
-            r=requests.post('https://lockeysuperserverdeluxe.azurewebsites.net/Sensor', data=data)
-            print(r)
-            print("Unlocked")
-        #update previous input
-        prev_input = input
-        #slight pause
-        time.sleep(0.10)
-except KeyboardInterrupt:
-    pass
-finally:
-    GPIO.cleanup()
+
+def get_input() -> None:
+    return GPIO.input(4)
+
+
+def send_api_message(locked: bool) -> None:
+    global API_URL, MAC_ADDRESS
+
+    data = {
+        "ID": MAC_ADDRESS,
+        "IsLocked": locked
+    }
+
+    response = requests.post(API_URL, data=data)
+    print(response)
+    print("Unocked" if not locked else 'Locked')
+
+
+def main() -> None:
+    global API_URL, DELAY
+
+    response = requests.get(API_URL)
+    print(response._content.decode("utf-8"))
+
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(4, GPIO.IN)
+
+    prev_input = get_input()
+
+    if prev_input == 0:
+        send_api_message(locked=False)
+    else:
+        send_api_message(locked=True)
+
+    time.sleep(DELAY)
+
+    try:
+        while True:
+            # take a reading
+            input = get_input()
+            print(input)
+
+            # if the last reading was low and this one high, alert us
+            if prev_input < input:
+                send_api_message(locked=True)
+
+            if prev_input > input:
+                send_api_message(locked=False)
+
+            # update previous input
+            prev_input = input
+
+            # slight pause
+            time.sleep(DELAY)
+
+    except KeyboardInterrupt:
+        pass
+    finally:
+        GPIO.cleanup()
+
+
+if __name__ == '__main__':
+    main()
